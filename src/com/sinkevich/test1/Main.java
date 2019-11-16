34a54c6a58089f86a050fdfb86204ec904bde177
rnsnkvch <rnsnkvch@gmail.com>
2019-11-16 22:03:11 +0300

    system_in

--- src/com/sinkevich/test1/Main.java
+++ src/com/sinkevich/test1/Main.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner se=new Scanner(System.in);
-        se.
+            if (se.hasNext()) {
+                int a=se.nextInt();
+                System.out.println(a);
+        }
         System.out.println("hello, world!");
     }
 }
