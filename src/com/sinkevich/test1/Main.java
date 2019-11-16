e8010c358fa10aa34f2264e5f355d09ec9502c80
rnsnkvch <rnsnkvch@gmail.com>
2019-11-16 21:42:05 +0300

    init

---
+++ src/com/sinkevich/test1/Main.java
@@ -0,0 +1,11 @@
+package com.sinkevich.test1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner se=new Scanner(System.in);
+        se.
+        System.out.println("hello, world!");
+    }
+}
